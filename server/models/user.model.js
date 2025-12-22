104173c876bbf003b842faa3fe7441956838316c
armasahar <armasahar.code@gmail.com>
2025-12-22 19:44:28 +0530

    worked on all the reviews of models

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,73 +1,78 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-
-  password: {
-    type: String,
-    required: true,
-  },
-
-  role: {
-    type: String,
-    enum: ["customer", "teller", "admin"],
-    required: true,
-  },
-
-  isActive: {
-      type: Boolean,
-      required: true
-    },
-
-      verifyToken: {
-    email: {
-      type: String,
-      default: null,
-    },
-    phone: {
-      type: String,
-      default: null,
-    },
-  },
-
-  verified: {
-    email: {
-      type: Boolean,
-      default: false,
-    },
-    phone: {
-      type: Boolean,
-      default: false,
-    },
-  },
-
-  lastLogin: {
-    type: Date,
-    required: true,
-  },
-
-
-  loginHistory: [
+const userSchema = new mongoose.Schema(
   {
-    timestamp: { type: Date },
-    ipAddress: { type: String },
-    status: {
+    email: {
       type: String,
-      enum: ["success", "failed"]
+      unique: true,
+      required: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["customer", "teller", "admin"],
+      required: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      required: true,
+    },
+
+    verifyToken: {
+      email: {
+        type: String,
+        default: null,
+      },
+      phone: {
+        type: String,
+        default: null,
+      },
+    },
+
+    verified: {
+      email: {
+        type: Boolean,
+        default: false,
+      },
+      phone: {
+        type: Boolean,
+        default: false,
+      },
+    },
+
+    lastLogin: {
+      type: Date,
+      required: true,
+    },
+
+    loginHistory: [
+      {
+        timestamp: { type: Date },
+        ipAddress: { type: String },
+        status: {
+          type: String,
+          enum: ["success", "failed"],
+        },
+      },
+    ],
+    createdAt:{
+        type: Date,
+        required: true
+    },
+    updatedAt:{
+        type: Date,
+        required: true
     }
-  }
-],
+  },
+//   { Timestamp: true }//this is giving us createdAt and updatedAt data;
+); 
 
- timestamp:{
-    type: Date
- }
-});
+const User = mongoose.model("User", userSchema);
 
-const userModel = mongoose.model("User", userSchema);
-
-export default userModel;
-T
+export default User;
