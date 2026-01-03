3b8e8fc99689fe8bf0e227fc8602e2e2593a4540
armasahar <armasahar.code@gmail.com>
2026-01-04 01:22:31 +0530

    Validations of transaction system

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import './dbConnect.js'
 import authRouter from './routes/user.route.js'
 import customerRouter from './routes/customer.route.js'
 import accountRouter from './routes/account.route.js'
+import transactionRouter from './routes/transaction.route.js'
 
 import dotenv from 'dotenv'
 
@@ -20,6 +21,7 @@ server.use(express.json())
 server.use('/api/auth', authRouter);
 server.use('/api/customers', customerRouter);
 server.use('/api/accounts', accountRouter);
+server.use('/api/transactions', transactionRouter)
 
 server.listen(PORT, ()=>{
         console.log(`Server is running at http://localhost:${PORT} 👍`);
