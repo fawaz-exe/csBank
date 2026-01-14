1f33e68c08de4f6f0f9717bc7410b4c3110ff569
OmerAhmedQuadri <omerquadri953@gmail.com>
2026-01-14 09:03:13 +0000

    added admin route to get all users

--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -2,7 +2,7 @@ import express from 'express'
 import authMiddleware from '../middlewares/auth.middleware.js'
 import adminOnly from '../middlewares/admin.middleware.js'
 
-import { deleteUserByAdmin, manageIpPolicy, createUserByAdmin, getLoginLogs } from '../controllers/admin.controller.js'
+import { deleteUserByAdmin, manageIpPolicy, createUserByAdmin, getLoginLogs, getAllUsers } from '../controllers/admin.controller.js'
 import ipPolicyMiddleware from '../middlewares/ipPolicy.middleware.js';
 
 const adminRouter = express.Router();
@@ -13,6 +13,7 @@ adminRouter.delete('/users/:id', deleteUserByAdmin);
 adminRouter.post('/ip-policies', manageIpPolicy);
 adminRouter.post('/users/create', createUserByAdmin);
 adminRouter.get('/login-logs', getLoginLogs)
+adminRouter.get('/all-users', getAllUsers);
 
 adminRouter.use((req, res) => {
     res.status(404).json({error: "Admin route not found ! ⚠️"});
